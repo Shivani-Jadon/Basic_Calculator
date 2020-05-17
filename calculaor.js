91f3f0bf655fde7dfaa7b181c68e87fcb4bb1a3e
Shivani <shivanijadon98@gmail.com>
2020-05-17 19:00:29 +0530

    add percentage in-place of mod and minor changes

--- calculaor.js
+++ calculaor.js
@@ -13,17 +13,36 @@ enter[0].addEventListener('click', function(){
 //for sign button
 enter[1].addEventListener('click', function(){
     var i = document.getElementById("input");
-    expression += "-";
-    i.innerText += "-";
+
+    // if the expression contains a - sign before it will be erased
+    if(expression[expression.length - 1] == "-"){
+        expression = expression.slice(0,-1) + "+";
+        i.innerText = expression;
+    }
+    else{
+        expression += "-";
+        i.innerText += "-";
+    }
+    
 });
 
 
 //for input of operands 0-9 and .
 for(let i=2; i<=12; i++)
 {
+    
     enter[i].addEventListener('click', function(){
+
         var operand = document.getElementById("input");
         var data = enter[i].getAttribute("data-value");
+
+        //if the screen prints error then it will be cleared off before printing new expresssion    
+        if(operand.innerText === "ERROR")
+        {
+            operand.innerText = "";
+            expression = "";
+        }
+
         expression += data;
         operand.innerText += data;
     });
@@ -42,8 +61,19 @@ for(let i=1; i<=5; i++){
     enter_opr[i].addEventListener('click', function(){
         let operator = document.getElementById("input");
         let value = enter_opr[i].getAttribute("data-value");
-        expression += value;
-        operator.innerText += value;
+        
+        // % is pressed thgen percantage will be calculated
+        if(value == "%")
+        {
+            expression = expression * 0.01 + "*";
+            operator.innerText = expression;
+        }
+        else
+        {
+            expression += value;
+            operator.innerText += value;
+        }
+
     });
 }
 
